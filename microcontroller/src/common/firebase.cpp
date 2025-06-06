81a0dc2d5961b8b01087df749451ff90bc0323bd
filipecarvalhodev <42754748+filipecarvalhodev@users.noreply.github.com>
2025-06-06 10:51:10 +0100

    Fixed firebase.cpp

--- microcontroller/src/common/firebase.cpp
+++ microcontroller/src/common/firebase.cpp
@@ -15,11 +15,19 @@ bool loadFirebase(const char *apiKey, const std::string email, const std::string
   Serial.printf("Firebase API Key: %s\n", apiKey);
   Serial.printf("Firestore Project : %s\n\n", FIREBASE_PROJECT);
 
-  if (!Firebase.signUp(&config, &auth, email, password))
+  if (email != "" && password != "")
   {
-    Serial.printf("Firebase anon sign-up failed: %s\n",
-                  config.signer.signupError.message.c_str());
-    return false;
+    auth.user.email = email;
+    auth.user.password = password;
+  }
+  else
+  {
+    if (!Firebase.signUp(&config, &auth, "", ""))
+    {
+      Serial.printf("Firebase anon sign-up failed: %s\n",
+                    config.signer.signupError.message.c_str());
+      return false;
+    }
   }
 
   config.token_status_callback = tokenStatusCallback;
